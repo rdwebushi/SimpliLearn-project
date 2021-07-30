909ef4921149fad8f3542579f5aaac7130a08153
Rushikesh Deshpande <rushikesh26deshpande@gmail.com>
2021-07-30 08:49:44 -0400

    project updated

--- Phase 1 project/inter.js
+++ Phase 1 project/inter.js
@@ -2,29 +2,28 @@ var count =1;
 function addData() {
     var name = document.getElementById("name").value;
     var ta = document.getElementById("ta").value; 
-    //alert("Event fired");
-    //This code is use to create the p tag
+    
+    //Code to create the p tag
     var myPTag = document.createElement("p");
-    //This code is use to create the content for any tags 
-    //var myPTagContent = document.createTextNode("Welcome to JavaScript");
-    var myPTagContent = document.createTextNode(" " +name+"  "+ta);
+    
+    
+    var myPTagContent = document.createTextNode("< " +name );
+    var myPTagContent1 = document.createTextNode(" "+ta);
+   
     // Adding the attribute for p tag
-    myPTag.setAttribute("style","color:black;font-size:24px;");
-  
-    myPTag.setAttribute("class","myClass");
-  
-    //  else {
-    //     myPTag.setAttribute("class","myClass1");    
-    // }
+    myPTag.setAttribute("style","color:black;font-size:24px;");  
+    myPTag.setAttribute("class","myClass");  
 
+ 
     myPTag.setAttribute("id",count);
      //Add the cotent to p tag;
      myPTag.appendChild(myPTagContent);
      document.getElementById("blogg").appendChild(myPTag);
-    reset();
-}
+     myPTag.appendChild(myPTagContent1);
+     document.getElementById("blogg").appendChild(myPTag);
 
-    
+    reset();
+}    
     
     function reset() {
         document.getElementById("name").value="";
